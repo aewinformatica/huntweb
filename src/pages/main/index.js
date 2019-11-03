33fe7c13ad8be0f9e53c048361485027112b1f7f
AewInformatica <aewinformatica@gmail.com>
2019-11-03 12:12:27 -0300

    Listando produtos

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import api from "../../services/api";
+import "./styles.css";
 
 export default class Main extends Component {
 
@@ -19,12 +20,19 @@ export default class Main extends Component {
     this.setState({products: response.data.docs});
   }
   render(){
+    //desestruturacao para buscar a variavel products dentro do state
+    const { products } = this.state;
     return (
       <div className= "product-list">
         {/* usando o map para percorrer a lista e criar um produto para cada item da lista */}
-        {this.state.products.map(product =>(
+        {products.map(product =>(
           /*usando map sempre informar key */
-          <h2 key={product._id}>{product.title}</h2>
+          <article key={product._id}>
+            <strong>{product.title}</strong>
+            <p>{product.description}</p>
+            
+            <a href="">Acessar</a>
+          </article>
         ))}
       </div>
     );
