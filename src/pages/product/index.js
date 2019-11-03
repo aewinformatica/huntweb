7c686fab5aac380816e213f36a5a43f6190c2776
AewInformatica <aewinformatica@gmail.com>
2019-11-03 15:08:13 -0300

    Navegando pro detalhe

--- src/pages/product/index.js
+++ src/pages/product/index.js
@@ -1,9 +1,35 @@
 import React, {Component} from  "react";
+import api from '../../services/api';
+
+import './styles.css';
 
 export default class Product  extends Component{
 
-  render(){
+  state = {
+    product: {}
+  };
 
-    return <h1>Product</h1>
+  async componentDidMount(){
+
+    // desestruturacao do dom
+    const { id } = this.props.match.params;
+
+    const response = await api.get(`/products/${id}`);
+
+    // preenchendo o objeto product do state com os dados encontrados
+    this.setState({ product : response.data });
+  }
+  render(){
+    const { product } = this.state;
+    return (
+      <div className="product-info">
+        <h1>{product.title}</h1>
+        <p>{product.description}</p>
+
+        <p>
+          URL: <a href={product.url}>{product.url}</a>
+        </p>
+      </div>
+    );
   }
 }
